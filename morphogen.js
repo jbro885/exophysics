d6de6fef7de8dd109fb45e91829e208f3751ac1b
hut <hut@hut.pm>
2020-10-17 02:55:15 +0200

    moving dots

--- morphogen.js
+++ morphogen.js
@@ -15,14 +15,14 @@ class Particle {
 
 function initBuffers(gl) {
     const positions = [
-        1.0,  1.0,
-        -1.0,  1.0,
-        1.0, -1.0,
-        -1.0, -1.0,
+        0.7,  0.4,
+        -0.2,  0.6,
+        0.3, -0.3,
+        -0.5, -0.9,
     ];
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
 
     var colors = [
         0.0,  1.0,  1.0,  1.0,
@@ -68,10 +68,14 @@ function drawScene(gl, programInfo, buffers) {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexColor,
         size, type, normalize, stride, offset);
-    gl.enableVertexAttribArray( programInfo.attribLocations.vertexColor);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
 
     gl.useProgram(programInfo.program);
 
+    nextFrame(gl, programInfo, buffers);
+}
+
+function nextFrame(gl, programInfo, buffers) {
     var transformBuffer = gl.createBuffer();
     var emptyDataArray = new Float32Array(999);
 
@@ -82,11 +86,11 @@ function drawScene(gl, programInfo, buffers) {
     gl.bufferData(gl.TRANSFORM_FEEDBACK_BUFFER, emptyDataArray, gl.STATIC_READ);
     gl.bindBuffer(gl.TRANSFORM_FEEDBACK_BUFFER, null);
     gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, transformBuffer);
-    gl.beginTransformFeedback(gl.TRIANGLES);
+    gl.beginTransformFeedback(gl.POINTS);
 
     var offset = 0;
     var vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
+    gl.drawArrays(gl.POINTS, offset, vertexCount);
 
     var count = 0;
     gl.endTransformFeedback();
@@ -94,16 +98,26 @@ function drawScene(gl, programInfo, buffers) {
 
     const fence = gl.fenceSync(gl.SYNC_GPU_COMMANDS_COMPLETE, 0);
     gl.flush();
-    console.log('waiting...');
     setTimeout(waitForResult);
     function waitForResult() {
         const status = gl.clientWaitSync(fence, 0, 0);
         if (status === gl.CONDITION_SATISFIED || status === gl.ALREADY_SIGNALED) {
             gl.deleteSync(fence);
-            const output = new Float32Array(vertexCount);
+            const output = new Float32Array(vertexCount * 2);
             gl.bindBuffer(gl.ARRAY_BUFFER, transformBuffer);
             gl.getBufferSubData(gl.ARRAY_BUFFER, 0, output);
-            console.log(output);
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(output), gl.DYNAMIC_DRAW);
+            var size = 2;
+            var type = gl.FLOAT;
+            var normalize = false;
+            var stride = 0;
+            var offset = 0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+            gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition,
+                size, type, normalize, stride, offset);
+            gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+            nextFrame(gl, programInfo, buffers);
         } else {
             setTimeout(waitForResult);
         }
